e730a446b9a6d336e6c94c37c21cba861cb50c1b
Katie Cunningham <katie.fulton@gmail.com>
2016-08-01 19:17:30 -0400

    Stuff

---
+++ README.md
@@ -0,0 +1,8 @@
+Give me a place to eat
+======================
+
+Seriously, this is a project that was created so that my husband and I can off-load the decision about where to eat to dinner to a random number generator. We tried offloading it onto the kids, but their taste is crap.
+
+Before you ask, yes, we know about wtfsigte.com, but its algorithm doesn't take into account that we're surrounded by nine-million fast-food joints.
+
+Eventually, I want to hook this up to an IFTTT command for my Echo, but until then, we're stuck with an aggressively plain web page.
---
+++ pte/static/wte.js
@@ -0,0 +1,33 @@
+restaurants = [
+    "All american steak house",
+    "Peruvian place",
+    "Toltecos",
+    "Not your average joe's",
+    "Firebird",
+    "Bungalow Alehouse",
+    "Ornery Brewery",
+    "Uncle Julio's",
+    "Burger Fi",
+    "Nando's Peri Peri",
+    "Travinia",
+    "Five Guys",
+    "Two Brothers",
+    "Astoria",
+    "Cock and Bowl",
+    "Bottle Stop",
+    "Secret Garden Cafe",
+    "Cheesecake factory",
+    "Hemibry",
+    "Dixie Bones",
+    "Smokey Bones",
+    "Taste of Tandoor",
+    "Siam Bistro",
+    "Chuy's",
+    "Matchbox",
+    "The Knife",
+    "Sakura",
+]
+
+var r = restaurants[Math.floor(Math.random() * restaurants.length)];
+
+$("span#restaurant").text(r)
